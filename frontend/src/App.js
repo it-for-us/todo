f2eedb0fd9de92a70dee13c462b06239195d1f4a
HarunAltun <altunhr@gmail.com>
2022-12-09 00:16:52 +0100

    Footer added to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Login from './components/Login.js';
-
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
       <Login />
+      <Footer />
     </div>
   );
 }
