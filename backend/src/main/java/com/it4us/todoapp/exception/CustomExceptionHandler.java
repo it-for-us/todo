95cdad9811b39c144bd407a427cd4659db64a533
Umut Doruk <umutdoruk1987@gmail.com>
2022-12-08 16:33:23 +0100

    implement create workspace API

--- backend/src/main/java/com/it4us/todoapp/exception/CustomExceptionHandler.java
+++ backend/src/main/java/com/it4us/todoapp/exception/CustomExceptionHandler.java
@@ -1,11 +1,15 @@
 package com.it4us.todoapp.exception;
 
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
@@ -16,22 +20,34 @@ public class CustomExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
     }
 
-    @ExceptionHandler({ Exception.class })
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGlobalException(Exception ex) {
         ErrorResponse errorResponse = new ErrorResponse("internal server error",500);
         return new ResponseEntity<>(errorResponse,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-//    @ExceptionHandler(UserNotExistException.class)
-//    public ResponseEntity<?> userNotExist(UserNotExistException userNotExistException){
-//        ErrorResponse errorResponse = new ErrorResponse(userNotExistException.getMessage(),404);
-//        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-//
-//    }
+    @ExceptionHandler(WorkspaceExistException.class)
+    public ResponseEntity<?> workspaceExist(WorkspaceExistException workspaceExistException){
+        ErrorResponse errorResponse = new ErrorResponse("workspace is already exist", 403);
+        return new ResponseEntity<>(errorResponse,HttpStatus.FORBIDDEN);
+    }
 
+    @ExceptionHandler(IncorrectWorkspaceNameException.class)
+    public ResponseEntity<?> incorrectWorkspaceName(IncorrectWorkspaceNameException incorrectWorkspaceNameException){
+        ErrorResponse errorResponse = new ErrorResponse("workspace name is in incorrect format", 400);
+        return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
+    }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> incorrectWorkspaceName(MethodArgumentNotValidException ex){
+        List<String> details = new ArrayList<>();
 
-
-
+        for(ObjectError error : ex.getBindingResult().getAllErrors()) {
+            details.add(error.getDefaultMessage());
+        }
+        String message = String.join(",", details);
+        ErrorResponse errorResponse = new ErrorResponse("workspace name is in incorrect format \n"+message, 400);
+        return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
+    }
 
 }
