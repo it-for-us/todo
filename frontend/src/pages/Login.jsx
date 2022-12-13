b29e49c6b233e5fa8779cb72f22ae83d0a1935e6
kemaltt <ttnckemal@gmail.com>
2022-12-13 19:36:11 +0100

    forgot password page link added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
