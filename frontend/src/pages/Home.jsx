3ab7b1c5c56a011b3b8cb675e240dab62ae4a6a4
HarunAltun <altunhr@gmail.com>
2022-12-18 23:55:12 +0100

    Home page created

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,24 +3,33 @@ import { useState } from "react";
 import Card from "@mui/material/Card";
 import { Container } from "@mui/system";
 import { Box, CardContent, Typography, Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import TextField from "@mui/material/TextField";
+import InputBase from "@mui/material/InputBase";
 import { useForm } from "react-hook-form";
-import Modal from "@mui/material/Modal";
-
+import Grid from "@mui/material/Grid";
+import { useNavigate } from "react-router-dom";
+import { redirect } from "react-router-dom";
 export default function Home() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
   const errorMessage =
-    "Workspace and Board names can contain letters (a-z),numbers(0-9),and an underline(_) and it can have 4-15 characters long. It canstart only letter or number";
+    "Workspace and Board names can contain letters (a-z), numbers(0-9), and an underline(_) and it can have 4-15 characters long. It can start only letter or number";
+  const inputError = (
+    <Typography
+      variant="subtitle2"
+      sx={{
+        color: "red",
+        bgcolor: "gray",
+        textAlign: "center",
+        borderRadius: "32px",
+      }}
+    >
+      Please try a valid name
+    </Typography>
+  );
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-
-  const navigate = useNavigate();
   const [userSticykNote, setUserStickyNote] = useState([
     {
       workSpaceName: "testSpace",
@@ -28,7 +37,6 @@ export default function Home() {
     },
   ]);
   console.log(userSticykNote);
-  console.log(errors.workSpaceName?.message);
 
   return (
     <Container
@@ -37,144 +45,147 @@ export default function Home() {
         mt: 8,
       }}
     >
-      <div sx={{ right: "0", bottom: "0" }}>
-        {errors.workSpaceName?.message || errors.boardName?.message
-          ? errorMessage
-          : ""}
-      </div>
-      <Typography
-        component="h1"
-        variant="h4"
+      <Grid
+        container
+        spacing={1}
         sx={{
-          borderRadius: "16px",
-          textAlign: "center",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          color: "white",
         }}
       >
-        Welcome DART
-      </Typography>
-      <Card
-        sx={{
-          bgcolor: "rgba(175,149,213,255)",
-          borderRadius: "16px",
-          width: "50%",
-          margin: "auto",
-          mt: 2,
-        }}
-      >
-        <CardContent
-          sx={{
-            textAlign: "center",
-          }}
-        >
-          <Typography sx={{ width: "75%", margin: "auto" }}>
-            Firstly create a workspace and a board than start add your post-it
-          </Typography>
+        <Grid item xs={3}></Grid>
 
-          <Container component="main" maxWidth="xs">
-            <Box
-              component="form"
-              textAlign="center"
-              noValidate
-              onSubmit={handleSubmit((data) => {
-                console.log(data);
-              })}
-            >
-              <TextField
-                sx={{
-                  bgcolor: "white",
-                  borderRadius: "16px",
-                  borderTopColor: "red",
-                  color: "white",
-                }}
-                margin="normal"
-                name="workSpaceName"
-                id="workspacename"
-                label="WorkSpace Name"
-                required
-                color="warning"
-                fullWidth
-                {...register("workSpaceName", {
-                  required: "Please try a valid name",
-                  minLength: { value: 4, message: "true" },
-                })}
-                autoFocus
-              />
-              <p>{errors.boardName?.message ? "true" : "No error"}</p>
-              <TextField
-                sx={{
-                  bgcolor: "white",
-                  borderRadius: "16px",
-                  borderTopColor: "red",
-                  color: "white",
-                }}
-                filled-basic
-                margin="normal"
-                required
-                fullWidth
-                name="BoardName"
-                id="boardName"
-                {...register("boardName", {
-                  required: "Board name needs 4 character",
-                  minLength: {
-                    value: 4,
-                    message: "true",
-                  },
-                })}
-              />
-              <Button
-                onClick={() => {
-                  if (
-                    errors.workSpaceName?.message ||
-                    errors.boardName?.message
-                  ) {
-                  }
-                }}
-                type="submit"
-                variant="contained"
-                sx={{ bgcolor: "#b498ce" }}
-              >
-                Create
-              </Button>
-            </Box>
-          </Container>
-        </CardContent>
-      </Card>
-
-      <div>
-        <Button
-          onClick={() => {
-            setOpen(true);
-          }}
-        >
-          Open modal
-        </Button>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box
+        <Grid item xs={6}>
+          <Typography
+            component="h1"
+            variant="h4"
             sx={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              width: 400,
-              bgcolor: "background.paper",
-              border: "2px solid #000",
-              boxShadow: 24,
-              p: 4,
+              textAlign: "center",
             }}
           >
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Workspace and Board names can contain letters (a-z),numbers(0-9),
-              and an underline(_) and it can have 4-15 characters long. It can
-              start only letter or number
+            Welcome DART
+          </Typography>
+          <Card
+            sx={{
+              bgcolor: "rgba(175,149,213,255)",
+              borderRadius: "16px",
+              margin: "auto",
+              mt: 2,
+            }}
+          >
+            <CardContent
+              sx={{
+                textAlign: "center",
+              }}
+            >
+              <Typography
+                sx={{
+                  margin: "auto",
+                }}
+              >
+                Firstly create a workspace and a board than start add your
+                post-it
+              </Typography>
+
+              <Container component="main" maxWidth="xs">
+                <Box
+                  component="form"
+                  textAlign="center"
+                  noValidate
+                  onSubmit={handleSubmit((data) => {
+                    navigate("/");
+                    console.log(data);
+                  })}
+                >
+                  <Typography
+                    variant="subtitle2"
+                    sx={{
+                      textAlign: "left",
+                      color: "white",
+                      mt: 2,
+                    }}
+                  >
+                    WORKSPACE NAME*
+                  </Typography>
+                  {errors.workSpaceName?.message ? inputError : ""}
+                  <InputBase
+                    sx={{
+                      bgcolor: "white",
+                      borderRadius: "16px",
+                    }}
+                    name="workSpaceName"
+                    id="workspacename"
+                    fullWidth
+                    autoFocus
+                    {...register("workSpaceName", {
+                      required: "true",
+                      pattern: {
+                        value: /^[a-zA-Z0-9][a-zA-Z0-9_]{3,14}$/,
+                        message: "true",
+                      },
+                    })}
+                  />
+                  <Typography
+                    variant="subtitle2"
+                    sx={{
+                      textAlign: "left",
+                      color: "white",
+                      mt: 2,
+                    }}
+                  >
+                    BOARD NAME*
+                  </Typography>
+
+                  {errors.boardName?.message ? inputError : ""}
+                  <InputBase
+                    sx={{
+                      bgcolor: "white",
+                      borderRadius: "16px",
+                    }}
+                    fullWidth
+                    autoFocus
+                    name="BoardName"
+                    id="boardName"
+                    {...register("boardName", {
+                      required: "true",
+                      pattern: {
+                        value: /^[a-zA-Z0-9][a-zA-Z0-9_]{3,14}$/,
+                        message: "true",
+                      },
+                    })}
+                  />
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{ bgcolor: "#b498ce", mt: 2 }}
+                  >
+                    Create
+                  </Button>
+                </Box>
+              </Container>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={3}>
+          <Box
+            sx={{
+              bgcolor: "rgba(175,149,213,255)",
+              borderRadius: "5%",
+              color: "black",
+              mt: 8,
+            }}
+          >
+            {" "}
+            <Typography component="h1" variant="body2">
+              {errors.workSpaceName?.message || errors.boardName?.message
+                ? errorMessage
+                : ""}
             </Typography>
           </Box>
-        </Modal>
-      </div>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
