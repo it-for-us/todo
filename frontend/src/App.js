14ed2b8ae689fb491a544127ce48a18733dc02ba
kemaltt <ttnckemal@gmail.com>
2022-12-26 02:35:42 +0100

    login page created #101

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import Login from "./pages/Login";
-// import Register from "./pages/Register";
+import Login from "./modules/auth//Login";
 import SignUp from "./modules/auth/SignUp";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
@@ -11,15 +10,31 @@ import ForgotPassword from "./pages/ForgotPassword";
 import CreateNewPass from "./pages/CreateNewPass";
 import Home from "./pages/Home";
 import { UserContextProvider } from "./contexts/UserContext";
+import { useState } from "react";
+
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  console.log(isLogin);
   return (
     <div className="App">
       <BrowserRouter>
         <UserContextProvider>
           {/* <Header /> */}
           <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
+            <Route
+              path="/login"
+              element={<Login setIsLogin={setIsLogin} isLogin={isLogin} />}
+            />
+            <Route
+              path="/"
+              element={
+                isLogin ? (
+                  <Home />
+                ) : (
+                  <Login setIsLogin={setIsLogin} isLogin={isLogin} />
+                )
+              }
+            />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/about" element={<About />} />
