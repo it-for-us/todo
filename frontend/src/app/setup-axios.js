340ada785e54e59d29579bee942f329d7c7cc891
kemaltt <ttnckemal@gmail.com>
2023-01-20 13:50:40 +0100

    landing page background changed

--- frontend/src/app/setup-axios.js
+++ frontend/src/app/setup-axios.js
@@ -1,27 +1,27 @@
-import axios from 'axios';
-import { API_URL } from '../constants/app-constants';
+import axios from "axios";
+import { API_URL } from "../constants/app-constants";
 
 export const axiosSetup = () => {
   axios.defaults.baseURL = API_URL;
-  axios.defaults.headers.common['Content-Type'] = 'application/json';
-  axios.defaults.timeout = 10000;
+  axios.defaults.headers.common["Content-Type"] = "application/json";
+  // axios.defaults.timeout = 10000;
 };
-
-
 
 export const authAxios = () => {
   const instance = axios.create({
     baseURL: API_URL,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     timeout: 10000,
   });
-  
+
   instance.interceptors.request.use(
     (config) => {
-      const storage = JSON.parse(sessionStorage.getItem('persist:dart-todo-app-auth'));
-      console.log(storage.token.split('"')[1])
+      const storage = JSON.parse(
+        sessionStorage.getItem("persist:dart-todo-app-auth")
+      );
+      console.log(storage.token.split('"')[1]);
       const token = storage.token ? storage.token.split('"')[1] : null;
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
@@ -32,14 +32,14 @@ export const authAxios = () => {
       Promise.reject(error);
     }
   );
-  
+
   instance.interceptors.response.use(
     (response) => {
       return response;
     },
     (error) => {
       if (error.response.status === 401) {
-        sessionStorage.removeItem('persist:dart-todo-app-auth');
+        sessionStorage.removeItem("persist:dart-todo-app-auth");
       }
       return Promise.reject(error);
     }
